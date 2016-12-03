75701d1a94b362684cfa9984be7baba840ff3185
aayusharora <aayush113002@gmail.com>
2016-12-03 18:56:07 +0530

    Ajax call Retrieval FourSquare Data

--- app/external/viewModel.js
+++ app/external/viewModel.js
@@ -2,8 +2,7 @@
 function AppViewModel() {
     var self = this;
     self.address = ko.observable('Delhi');
-    self.latitude = '';
-    
+   
     this.getUrl = ko.computed(function(address) {
 
       var url = "https://maps.googleapis.com/maps/api/geocode/json?address="+self.address()+"&key=AIzaSyAlQWLkSPjKEvBBbMkVZjBtIminATljqis";  console.log(url);
@@ -12,7 +11,8 @@ function AppViewModel() {
         if (this.readyState == 4 && this.status == 200) {
           
              var data = JSON.parse(this.responseText).results[0].geometry.location;
-             self.latitude = data.lat;
+              
+            self.ajaxCall(data.lat, data.lng);
              //self.sendData(latitude,longitude);
         }
       };
@@ -21,7 +21,43 @@ function AppViewModel() {
       console.log(self.latitude);
     })
 
+    this.ajaxCall = function (lat, lng) {
+       var coordinate = {
+          "latitude" : lat,
+          "longitude" : lng
+       }
+
+      $.ajax({
+          type: "POST",
+          url:"http://localhost:3000",
+          data: JSON.stringify(coordinate),
+          contentType: 'application/json',
+          sucess: function(){
+              alert("something!");
+          },
+          error: function(textstatus, errorThrown) {
+              alert('text status ' + textstatus + ', err ' + errorThrown);
+          }
+      });
+
+    }
+
+
+    /*this.makeAjaxCall = function (methodType, url ,callback) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.open(methodType, url, true);
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) { 
+            callback(xhr.response) 
+        }
    
+       xhttp.send();
+       console.log("request sent to the server");
+      };  
+          
+    }
+*/
+  
  }
 
 ko.applyBindings(new AppViewModel());
--- request.js
+++ request.js
@@ -2,23 +2,38 @@ var express = require('express')
 var app = express()     // define our app using express
 var request = require('request');
 
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000; 
+app.use(bodyParser.json());                        
+
+    // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('app'));
 
+//app.use(express.bodyParser());
+
 app.listen(port);
 
-app.get('/map', function(req, res, next) {
+app.post('/', function(req,res) {
+  
+    var data = coordinates(req.body.latitude, req.body.longitude);
+    console.log(data);
+    res.send('ok');
 
-  var username = req.query.name;
-  console.log(username);
+})
+
+function coordinates(lat, lng) {
+  console.log(lat);
+  console.log(lng);
+ 
   var CLIENT_ID = "4KRLZJS2EXSKBHNSGXGHJIPQQG3LAFRTFZ22XBQ2XTXCUJMS";
   var CLIENT_SECRET = "BVD4TUFEPUY4IX021WFIIWCZTN3UY0RQWMKTSA3CP5IFHZCK"; 
   var url = "https://api.foursquare.com/v2/venues/search?client_id="+ CLIENT_ID +
    "&client_secret="+ CLIENT_SECRET +
-    "&v=20130815&ll=28.881338,77.34845780000001&query=sushi";
+    "&v=20130815&ll="+lat+","+lng+"&query=sushi";
 
-
+  console.log(url);
   request(url, function (error, response, body) {
       //Check for error
       //console.log(url);
@@ -32,12 +47,17 @@ app.get('/map', function(req, res, next) {
           return console.log('Invalid Status Code Returned:', response.statusCode);
       }
 
+      else {
+        console.log(response);
+        console.log(body);
+      }
+    
       //All is good. Print the body
-      res.send(body); // Show the HTML for the Modulus homepage.
+       // Show the HTML for the Modulus homepage.
 
   })
 
-});
+};
 
 app.listen();
 console.log('Express server started on port %s', port);
